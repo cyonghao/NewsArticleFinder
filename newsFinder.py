9fe5aa0384b716f43f1d331fcf29acfd42f1d9aa
Yong Hao <yonghao.choo@gmail.com>
2018-09-29 10:22:10 +0800

    user inputs

--- newsFinder.py
+++ newsFinder.py
@@ -1,1 +1,18 @@
+from newsapi import NewsApiClient
 from tokenconfig import TOKEN
+
+def main():
+    # User input for country of source
+    country = str(input("Enter the country of choice:  "))
+
+    # User input for language of source
+    language = str(input("Enter the language of choice:  "))
+
+    # Displays list of sources based on country of choice
+    sources = newsapi.get_sources(country=country, language=language)
+
+if __name__ == "__main__":
+    # Init
+    newsapi = NewsApiClient(api_key=TOKEN)
+
+    main()
