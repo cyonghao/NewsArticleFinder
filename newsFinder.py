9de65855aa6d6f5c73799e59f78e467940d05f0a
Yong Hao <yonghao.choo@gmail.com>
2018-09-29 10:53:46 +0800

    user input for keywords

--- newsFinder.py
+++ newsFinder.py
@@ -2,21 +2,24 @@ from newsapi import NewsApiClient
 from tokenconfig import TOKEN
 
 def main():
-    # User input for country of source
-    country = str(input("Enter the country of choice:  "))
+    # # User input for country of source
+    # country = str(input("Enter the country of choice:  "))
 
-    # User input for language of source
-    # language = str(input("Enter the language of choice:  "))
+    # # Displays list of sources based on country of choice
+    # avail_sources = newsapi.get_sources(country=country)
+    # avail_sources = avail_sources['sources']
+    # for avail_source in avail_sources:
+    #     print(avail_source['id'])
 
-    # Displays list of sources based on country of choice
-    avail_sources = newsapi.get_sources(country=country)
-    avail_sources = avail_sources['sources']
-    for avail_source in avail_sources:
-        print(avail_source['id'])
+    # # User input for selected sources
+    # selected_sources = str(input("Enter the source(s) of your choice, each separated with a comma:   "))
+    # selected_sources = selected_sources.strip().replace(" ","").lower()
 
-    selected_sources = str(input("Enter the source(s) of your choice, each separated with a comma:   "))
-    selected_sources = [selected_source.strip() for selected_source in selected_sources.split(',')]
-    print(selected_sources)
+    # User input for selected keywords/ phrases
+    keywords = str(input("Enter keywords/ phrases to search for, each separated with a comma:  "))
+    keywords = ",".join([keyword.strip().lower() for keyword in keywords.split(",")])
+    
+    
 
 
 if __name__ == "__main__":
